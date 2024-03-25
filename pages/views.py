e6bd6b02470ea3870e464091c7dd4876f8caf07f
ubu <theonlycodingbear@gmail.com>
2024-03-25 13:32:55 +0300

    commit 1

---
+++ pages/views.py
@@ -0,0 +1,15 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+
+# def homePageView(request):
+#     return HttpResponse("Hello, World!")
+
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):  
+    template_name = "about.html"
