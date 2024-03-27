cb409f103cf78a53c831cd444ecd387dff7d2b51
ubu <theonlycodingbear@gmail.com>
2024-03-28 00:01:31 +0300

    commit 6

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = "home.html"
-
-class AboutPageView(TemplateView):  
-    template_name = "about.html"
+    
